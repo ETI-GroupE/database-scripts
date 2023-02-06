0b27d7cf2bf1db15f4a81016a4e3906ab1886697
NPLeeWenKang <s10203100@connect.np.edu.sg>
2023-02-06 20:05:30 +0800

    update feedback db

--- feedback/mysql.sql
+++ feedback/mysql.sql
@@ -3,16 +3,17 @@ DROP TABLE IF EXISTS feedback;
 
 -- Create Tables
 CREATE TABLE feedback (
+    feedback_id int auto_increment,
     created_at_date DATE DEFAULT (CURRENT_DATE),
     user_id int,
     product_id int,
-    rating int CHECK(rating >= 0 AND rating <= 5),
+    rating int CHECK(rating >= 1 AND rating <= 5),
     description text,
-    PRIMARY KEY(user_id, product_id)
+    PRIMARY KEY(feedback_id)
 );
 
 INSERT INTO feedback(user_id, product_id, rating, description) VALUES
 (1, 1, 5, 'Good'),
 (2, 1, 5, 'Good product'),
 (3, 1, 3, 'Good product but not worth the price'),
-(1, 2, 0, 'Product is broken and customer service is bad');
+(1, 2, 1, 'Product is broken and customer service is bad');
